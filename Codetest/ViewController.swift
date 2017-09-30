e57f30c47a18a4aa137cc9647ff1b21b0e9b0d53
kimpham1234 <kim.g.pham@sjsu.edu>
2017-09-30 15:52:09 -0700

    Initial Commit

---
+++ Codetest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Codetest
+//
+//  Created by Kim Pham on 9/30/17.
+//  Copyright © 2017 BooBoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
