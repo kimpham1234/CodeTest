d3506350398788dc2e54f24594ff1f1606d9633f
kimpham1234 <kim.g.pham@sjsu.edu>
2017-09-30 17:34:15 -0700

    get user repos

--- Codetest/ViewController.swift
+++ Codetest/ViewController.swift
@@ -7,9 +7,50 @@
 //
 
 import UIKit
+import Octokit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var repotable: UITableView!
+    var username = ""
+    var reponames = [String]()
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func getInfoButton(_ sender: UIButton) {
+        username = usernameTextField.text!
+        Octokit().repositories(owner: username){response in
+            switch response {
+            case .success(let repositories):
+                for repo in repositories{
+                    self.reponames.append(repo.name!)
+                }
+                print(self.reponames[0])
+                DispatchQueue.main.async {
+                    self.repotable.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        
+        }
+        
+    }
+    
+    @IBOutlet weak var infoTextView: UITextView!
+    
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reponames.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = reponames[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +61,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func updateUI(info: String){
+        infoTextView.text = info
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.resignFirstResponder()
+        return true
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+
+target 'Codetest' do
+  use_frameworks!
+  pod 'OctoKit.swift', '0.7.4'
+
+end
